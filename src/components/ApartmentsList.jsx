ee5db7aaea4a3277b58672be03e26faf8cc36327
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-09-15 13:31:35 +0200

    (it.2) ApartmentsList

--- src/components/ApartmentsList.jsx
+++ src/components/ApartmentsList.jsx
@@ -1,6 +1,34 @@
-function ApartmentsList(){
-    return(
-        <h1>this is the ApartmentsList component</h1>
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function ApartmentsList() {
+
+    const [apartments, setApartments] = useState(null);
+
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_URL}/apartments`)
+            .then(response => {
+                setApartments(response.data);
+            })
+            .catch(e => {
+                console.log("error getting apartments from the api...", e)
+            })
+    }, []);
+
+    return (
+        <>
+            <h1>List of Apartments:</h1>
+            {apartments && apartments.map((element) => {
+                return (
+                    <div key={element._id} className="apartment-summary">
+                        <h3>{element.title}</h3>
+                        <img src={element.img} alt={element.title} />
+                        <p>Price: {element.pricePerDay}</p>
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
