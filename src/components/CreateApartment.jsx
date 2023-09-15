c1e93faf6303bc4c92b45f329fc28d6277755a9f
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-09-15 18:10:58 +0200

    (it. 3) create new Apartment

--- src/components/CreateApartment.jsx
+++ src/components/CreateApartment.jsx
@@ -1,6 +1,75 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 function CreateApartment(){
+
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+    const [imageURL, setImageURL] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const newApartment = {
+            img: imageURL,
+            title: title,
+            pricePerDay: price
+        }
+
+        axios.post(`${import.meta.env.VITE_API_URL}/apartments`, newApartment)
+            .then( response => {
+                navigate("/apartments");
+            })
+            .catch( e => {
+                console.log("error creating new apartment...", e);
+            })
+    }
+
     return(
-        <h1>this is the CreateApartment component</h1>
+        <form onSubmit={handleSubmit}>
+
+            <label>
+                Title:
+                <input 
+                    type="text" 
+                    name="title" 
+                    required={true} 
+                    value={title} 
+                    onChange={(e) => { setTitle(e.target.value) }}
+                />
+            </label>
+
+
+            <label>
+                Price:
+                <input
+                    type="number"
+                    min={1}
+                    max={999}
+                    name="price"
+                    required={true}
+                    value={price}
+                    onChange={(e) => { setPrice(e.target.value) }}
+                />
+            </label>
+
+
+            <label>
+                Image URL:
+                <input
+                    type="text"
+                    name="imageURL"
+                    required={true}
+                    value={imageURL}
+                    onChange={(e) => { setImageURL(e.target.value) }}
+                />
+            </label>
+
+            <button>Create</button>
+        </form>
     )
 }
 
